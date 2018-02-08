423987cc9eb9d8c649f25eb87574123103753f7e
bharani <bharanigramesh28@gmail.com>
2018-02-08 11:57:12 +0530

    hi

---
+++ src/F.java
@@ -0,0 +1,10 @@
+
+public class F {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "./software/geckodriver.exe");
+		//WebDriver driver=new FirefoxDriver();
+
+	}
+
+}
